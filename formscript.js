b4f0473079519d0cef9148638c1b353c72789434
Cole <coltonflather@gmail.com>
2024-03-03 15:00:33 -0500

    added a save to text file in the fun facts page

--- formscript.js
+++ formscript.js
@@ -1,11 +1,11 @@
-document.getElementById('funFactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting normally
- 
+document.getElementById('funFactForm').addEventListener('submit', async function(event) {
+    await event.preventDefault(); // Prevent form from submitting normally
+    await downloadAsTxt()
  
     // Get form values
-    var name = document.getElementById('name').value;
-    var funFact = document.getElementById('funFact').value;
-    var consent = document.getElementById('publishConsent').checked;
+    var name = await document.getElementById('name').value;
+    var funFact = await document.getElementById('funFact').value;
+    var consent = await document.getElementById('publishConsent').checked;
  
  
     // Simple validation & action
@@ -25,8 +25,7 @@ document.getElementById('funFactForm').addEventListener('submit', function(event
     console.log('Name:', name);
     console.log('Fun Fact:', funFact);
     console.log('Consent:', consent ? 'Yes' : 'No');
- 
- 
+    
     alert('Thank you for submitting your fun fact!');
     window.location.replace("funfacts.html");
  });
@@ -35,4 +34,39 @@ document.getElementById('funFactForm').addEventListener('submit', function(event
  //function getFront() {
    // window.location.href = 'funfacts.html';
  //}
+ function getData(){
+    return {
+        "name": document.getElementById("name").value,
+        "fact": document.getElementById("funFact").value,
+        "consent": document.getElementById("publishConsent").checked
+    }
+ }
+ let globalHandle;
+async function writeFile(contents){
+    console.log(globalHandle)
+    const writable = await globalHandle.createWritable();
+    await writable.write(contents)
+    await writable.close()
+ }
+ async function downloadAsTxt(){
+    //make a new text file
+    globalHandle = await makeNewFile()
+    await writeFile(JSON.stringify(getData()))
+ }
+async function makeNewFile(){
+    const options = {
+        types: [
+            {
+                description: 'Text Files',
+                accept:{
+                    'text/plain':[".txt"],
+                },
+            },
+        ],
+    }
+    const handle = await window.showSaveFilePicker(options)
+    globalHandle = handle
+    console.log(globalHandle)
+    return handle
+ }
  
